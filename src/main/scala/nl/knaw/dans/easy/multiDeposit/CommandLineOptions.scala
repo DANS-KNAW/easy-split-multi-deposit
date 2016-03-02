35625d21cb7b5a7c8304c00fd50e257736f8f3b2
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-03-02 10:46:27 +0100

    Improved error messages in cmd parser

--- src/main/scala/nl/knaw/dans/easy/multiDeposit/CommandLineOptions.scala
+++ src/main/scala/nl/knaw/dans/easy/multiDeposit/CommandLineOptions.scala
@@ -19,10 +19,8 @@ import java.io.File
 
 import com.typesafe.config.{Config, ConfigFactory}
 import nl.knaw.dans.easy.multideposit.CommandLineOptions._
-import org.rogach.scallop.{ ScallopConf, singleArgConverter}
+import org.rogach.scallop.{ScallopConf, singleArgConverter}
 import org.slf4j.LoggerFactory
-
-import scala.util.Properties
 
 object CommandLineOptions {
 
@@ -40,7 +38,8 @@ object CommandLineOptions {
       // a good thing here?
       appHomeDir = Option(System.getProperty("app.home"))
         .map(new File(_))
-        .getOrElse(throw new IllegalArgumentException("The property \"app.home\" needs to be set.")),
+        .getOrElse(throw new IllegalArgumentException("The property 'app.home' is not available " +
+          "in the system properties.")),
       multidepositDir = opts.multiDepositDir(),
       springfieldInbox = opts.springfieldInbox(),
       outputDepositDir = opts.outputDepositDir())
@@ -55,6 +54,18 @@ class ScallopCommandLine(conf: Config, args: Array[String]) extends ScallopConf(
   import nl.knaw.dans.easy.multideposit.{CommandLineOptions => cmd}
 
   val fileMayNotExist = singleArgConverter(new File(_))
+  val fileShouldExist = singleArgConverter(filename => {
+    val file = new File(filename)
+    if (!file.exists) {
+      log.error(s"The directory '$filename' does not exist")
+      throw new IllegalArgumentException(s"The directory '$filename' does not exist")
+    }
+    if (!file.isDirectory) {
+      log.error(s"'$filename' is not a directory")
+      throw new IllegalArgumentException(s"'$filename' is not a directory")
+    }
+    else file
+  })
 
   printedName = "easy-split-multi-deposit"
   version(s"$printedName ${Version()}")
@@ -67,21 +78,22 @@ class ScallopCommandLine(conf: Config, args: Array[String]) extends ScallopConf(
   lazy val multiDepositDir = trailArg[File](name = "multi-deposit-dir", required = true,
     descr = "Directory containing the Submission Information Package to process. "
       + "This must be a valid path to a directory containing a file named "
-      + s"'${cmd.mdInstructionsFileName}' in RFC4180 format.")
+      + s"'${cmd.mdInstructionsFileName}' in RFC4180 format.")(fileShouldExist)
   validateOpt(multiDepositDir)(_.map(file =>
-      if (!file.isDirectory)
-        Left(s"Not a directory '$file'")
-      else if (!file.directoryContains(new File(file, cmd.mdInstructionsFileName)))
-        Left("No instructions file found in this directory, expected: "
-          + s"${new File(file, cmd.mdInstructionsFileName)}")
-      else
-        Right(()))
-      .getOrElse(Left("Could not parse parameter multi-deposit-dir")))
+    if (!file.isDirectory) {
+      Left(s"Not a directory '$file'")
+    }
+    else if (!file.directoryContains(new File(file, cmd.mdInstructionsFileName)))
+      Left("No instructions file found in this directory, expected: "
+        + s"${new File(file, cmd.mdInstructionsFileName)}")
+    else
+      Right(()))
+    .getOrElse(Left("Could not parse parameter multi-deposit-dir")))
 
   lazy val springfieldInbox = opt[File]("springfield-inbox",
     descr = "The inbox directory of a Springfield Streaming Media Platform installation. " +
       "If not specified the value of springfield-inbox in application.properties is used.",
-    default = Some(new File(conf.getString("springfield-inbox"))))
+    default = Some(new File(conf.getString("springfield-inbox"))))(fileShouldExist)
   validateOpt(springfieldInbox)(_.map(file =>
     if (!file.isDirectory)
       Left(s"Not a directory '$file'")
