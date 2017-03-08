f2233b8b0252ef3344bfda8481a268e863860209
Richard van Heest <richard.van.heest@dans.knaw.nl>
2017-03-08 12:12:08 +0100

    only do theses moves when the arguments do not contain --version or --help

--- run.sh
+++ run.sh
@@ -22,18 +22,20 @@ OUTPUT=data/output
 SPRINGFIELD=data/springfield-inbox
 STAGING=data/staging
 
-if [ "$(ls -A $OUTPUT)" ]; then
-    NEWOUTPUT=$OUTPUT-`date  +"%Y-%m-%d@%H:%M:%S"`
-    mv $OUTPUT $NEWOUTPUT
-    mkdir $OUTPUT
-    echo "the old output folder has been moved to $NEWOUTPUT"
-fi
+if [[ " ${ARGS[*]} " != *"--help"* ]] && [[ " ${ARGS[*]} " != *"--version"* ]]; then
+    if [ "$(ls -A $OUTPUT)" ]; then
+        NEWOUTPUT=$OUTPUT-`date  +"%Y-%m-%d@%H:%M:%S"`
+        mv $OUTPUT $NEWOUTPUT
+        mkdir $OUTPUT
+        echo "the old output folder has been moved to $NEWOUTPUT"
+    fi
 
-if [ "$(ls -A $SPRINGFIELD)" ]; then
-    NEWSPRINGFIELD=$SPRINGFIELD-`date  +"%Y-%m-%d@%H:%M:%S"`
-    mv $SPRINGFIELD $NEWSPRINGFIELD
-    mkdir $SPRINGFIELD
-    echo "the old springfield-inbox folder has been moved to $NEWSPRINGFIELD"
+    if [ "$(ls -A $SPRINGFIELD)" ]; then
+        NEWSPRINGFIELD=$SPRINGFIELD-`date  +"%Y-%m-%d@%H:%M:%S"`
+        mv $SPRINGFIELD $NEWSPRINGFIELD
+        mkdir $SPRINGFIELD
+        echo "the old springfield-inbox folder has been moved to $NEWSPRINGFIELD"
+    fi
 fi
 
 mvn exec:java -Dapp.home=$APPHOME \
