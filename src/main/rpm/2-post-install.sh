2e65427894f407c531e0413c7b318370979acd5a
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-01-11 12:58:14 +0100

    install stuff

--- src/main/rpm/2-post-install.sh
+++ src/main/rpm/2-post-install.sh
@@ -21,6 +21,7 @@ NUMBER_OF_INSTALLATIONS=$1
 MODULE_NAME=easy-split-multi-deposit
 INSTALL_DIR=/opt/dans.knaw.nl/$MODULE_NAME
 PHASE="POST-INSTALL"
+INBOX_DIR=/var/opt/dans.knaw.nl/tmp/easy-split-multi-deposit-inbox
 STAGING_DIR=/var/opt/dans.knaw.nl/tmp/easy-split-multi-deposit-staging
 
 echo "$PHASE: START (Number of current installations: $NUMBER_OF_INSTALLATIONS)"
@@ -28,6 +29,13 @@ echo "$PHASE: START (Number of current installations: $NUMBER_OF_INSTALLATIONS)"
 service_install_initd_service_script "$INSTALL_DIR/install/$MODULE_NAME-initd.sh" $MODULE_NAME
 service_install_systemd_unit "$INSTALL_DIR/install/$MODULE_NAME.service" $MODULE_NAME "$INSTALL_DIR/install/override.conf"
 service_create_log_directory $MODULE_NAME
+
+if [ ! -d ${INBOX_DIR} ]; then
+    echo -n "Creating inbox directory..."
+    mkdir -p ${INBOX_DIR}
+    chmod 777 ${INBOX_DIR}
+    echo "OK"
+fi
 
 if [ ! -d ${STAGING_DIR} ]; then
     echo -n "Creating staging directory..."
