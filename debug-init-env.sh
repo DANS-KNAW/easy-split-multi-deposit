45e67b19634c33a23030f2e06f5f7c22bc844090
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-01-11 11:15:07 +0100

    add zipping the test bags

--- debug-init-env.sh
+++ debug-init-env.sh
@@ -24,6 +24,14 @@ mkdir ${DATADIR}/input
 cp -r src/test/resources/allfields/input ${DATADIR}/input/allfields
 cp -r src/test/resources/invalidCSV/input ${DATADIR}/input/invalidCSV
 
+echo "Create zips for the test input"
+pushd ${DATADIR}/input/allfields
+zip -r ../allfields.zip *
+popd
+pushd ${DATADIR}/input/invalidCSV
+zip -r ../invalidCSV.zip *
+popd
+
 echo "Creating staging and output directories in $DATADIR..."
 mkdir ${DATADIR}/staging
 mkdir ${DATADIR}/output
