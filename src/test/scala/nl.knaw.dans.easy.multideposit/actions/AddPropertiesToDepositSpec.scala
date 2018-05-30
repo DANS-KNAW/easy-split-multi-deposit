919525c26cfa6e9c0c2ddc6c429bc47734947dfa
gulcin <gulcin.ermis@dans.knaw.nl>
2018-05-31 00:01:44 +0200

    some unit tests

--- src/test/scala/nl.knaw.dans.easy.multideposit/actions/AddPropertiesToDepositSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.multideposit/actions/AddPropertiesToDepositSpec.scala
@@ -15,13 +15,13 @@
  */
 package nl.knaw.dans.easy.multideposit.actions
 
-import java.util.UUID
-
+import better.files.File
 import nl.knaw.dans.easy.multideposit.TestSupportFixture
 import nl.knaw.dans.easy.multideposit.model.AudioVideo
-import org.joda.time.DateTime
+import org.joda.time.{ DateTime, DateTimeZone }
 import org.scalatest.BeforeAndAfterEach
 
+import scala.util.Properties.userHome
 import scala.util.Success
 
 class AddPropertiesToDepositSpec extends TestSupportFixture with BeforeAndAfterEach {
@@ -37,10 +37,8 @@ class AddPropertiesToDepositSpec extends TestSupportFixture with BeforeAndAfterE
   }
 
   "addDepositProperties" should "generate the properties file and write the properties in it" in {
-    //val y = new DateTime()
-    val y = DateTime.parse("2015-05-19")
-    val z = UUID.fromString("1de3f841-0f0d-048b-b3db-4b03ad4834d7")
-    action.addDepositProperties(testInstructions1.copy(audioVideo = AudioVideo()).toDeposit(), datamanagerId, "dm@test.org", depositId, /*y,*/ Some(z)) shouldBe a[Success[_]]
+
+    action.addDepositProperties(testInstructions1.copy(audioVideo = AudioVideo()).toDeposit(), datamanagerId, "dm@test.org", depositId, testInstructions1.baseUUID) shouldBe a[Success[_]]
 
     val props = stagingPropertiesFile(testInstructions1.depositId)
     props.toJava should exist
@@ -60,10 +58,8 @@ class AddPropertiesToDepositSpec extends TestSupportFixture with BeforeAndAfterE
   }
 
   it should "generate the properties file with springfield fields and write the properties in it" in {
-    //val y = new DateTime()
-    val y = DateTime.parse("2015-05-19")
-    val z = UUID.fromString("1de3f841-0f0d-048b-b3db-4b03ad4834d7")
-    action.addDepositProperties(testInstructions1.toDeposit(), datamanagerId, "dm@test.org", depositId, /*y,*/ Some(z)) shouldBe a[Success[_]]
+
+    action.addDepositProperties(testInstructions1.toDeposit(), datamanagerId, "dm@test.org", depositId, testInstructions1.baseUUID) shouldBe a[Success[_]]
 
     val props = stagingPropertiesFile(testInstructions1.depositId)
     props.toJava should exist
@@ -80,6 +76,44 @@ class AddPropertiesToDepositSpec extends TestSupportFixture with BeforeAndAfterE
         include("springfield.collection=Jans-test-files") and
         include("springfield.playmode=menu") and
         include regex "bag-store.bag-id=[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"
+    }
+  }
+
+  it should "generate the csv file in home directory" in {
+
+    action.csvPrinterToFile.close()
+
+    action.addDepositProperties(testInstructions1.copy(audioVideo = AudioVideo()).toDeposit(), datamanagerId, "dm@test.org", depositId, testInstructions1.baseUUID)
+
+    var curr = action.currentTimestamp
+
+    File(s"$userHome/easy-split-multi-deposit-identifier-info-$curr.csv").exists shouldBe true
+
+    File(s"$userHome/easy-split-multi-deposit-identifier-info-$curr.csv").contentAsString should {
+      include(testInstructions1.baseUUID.get.toString) and
+      include(depositId)
+    }
+
+    action.csvPrinterToFile.close()
+
+    val depositId2 = "deposit1"
+    val datamanagerId2 = "dm"
+    val action2 = new AddPropertiesToDeposit
+
+    action2.addDepositProperties(testInstructions1.copy(audioVideo = AudioVideo()).toDeposit(), datamanagerId2, "dm@test.org", depositId2, testInstructions1.baseUUID)
+
+    val depositId3 = "deposit2"
+    val datamanagerId3 = "dm"
+    val action3 = new AddPropertiesToDeposit
+
+    action2.addDepositProperties(testInstructions1.copy(audioVideo = AudioVideo()).toDeposit(), datamanagerId3, "dm@test.org", depositId3, testInstructions1.baseUUID)
+
+    val curr2 = action2.currentTimestamp
+
+    File(s"$userHome/easy-split-multi-deposit-identifier-info-$curr2.csv").contentAsString should {
+      include(testInstructions1.baseUUID.get.toString) and
+        include("deposit1") and
+      include("deposit2")
     }
   }
 }
